b57ad8e274df6d728b90b78bb2436b4d83ea8de4
Akinobu Mita <akinobu.mita@gmail.com>
2010-02-19 15:31:16 +0900

    update multimap-memcachedb-test

--- multimap-memcachedb-test.c
+++ multimap-memcachedb-test.c
@@ -8,6 +8,7 @@
 #include <getopt.h>
 #include <limits.h>
 #include <sys/time.h>
+#include <libmemcached/memcached.h>
 
 static void die(const char *err, ...)
 {
@@ -18,7 +19,17 @@ static void die(const char *err, ...)
 	va_end(params);
 }
 
-#include <libmemcached/memcached.h>
+static void *xmalloc(size_t size)
+{
+	void *ptr;
+
+	ptr = malloc(size);
+	if (!ptr)
+		die("malloc: out of memory");
+
+	return ptr;
+}
+
 
 static void do_memcached_set(memcached_st *memc, const char *key,
 		size_t key_length, const char *value, size_t value_length)
@@ -30,11 +41,19 @@ static void do_memcached_set(memcached_st *memc, const char *key,
 		warnx("memcached_set: %s", memcached_strerror(memc, ret));
 }
 
-static void run(const char *server)
+/*
+ * delete, set, mget
+ */
+static void run(const char *server, unsigned long requests,
+		unsigned long value_length)
 {
 	struct memcached_st memc;
 	struct memcached_server_st *servers;
+	const char *key = "key";
+	const size_t key_len = strlen(key);
+	char *value = xmalloc(value_length);
 	memcached_return ret;
+	int i;
 
 	memcached_create(&memc);
 	servers = memcached_servers_parse(server);
@@ -43,43 +62,64 @@ static void run(const char *server)
 	if (ret != MEMCACHED_SUCCESS)
 		die("memcached_server_push: %s", memcached_strerror(&memc, ret));
 
-	do_memcached_set(&memc, "key", 3, "1", 2);
-	do_memcached_set(&memc, "key", 3, "2", 2);
-	do_memcached_set(&memc, "key", 3, "3", 2);
 
-	const char *keys[] = { "key", };
-	const size_t key_length[] = { 3, };
+	ret = memcached_delete(&memc, key, key_len, 0);
+	if (ret != MEMCACHED_SUCCESS)
+		warnx("memcached_delete: %s", memcached_strerror(&memc, ret));
 
-	ret = memcached_mget(&memc, keys, key_length, 1);
-	if (ret != MEMCACHED_SUCCESS) {
+	for (i = 0; i < requests; i++)
+		do_memcached_set(&memc, key, key_len, value, value_length);
+
+	const char *keys[] = { key, };
+	const size_t key_lens[] = { key_len, };
+
+	ret = memcached_mget(&memc, keys, key_lens, 1);
+	if (ret != MEMCACHED_SUCCESS)
 		warnx("memcached_mget: %s", memcached_strerror(&memc, ret));
-	}
 
-	do {
-		char *value;
-		size_t value_length;
-		uint32_t flags;
-
-		value = memcached_fetch(&memc, NULL, NULL, &value_length, &flags, &ret);
+	char *v;
+	size_t length;
+	uint32_t flags;
+	for (i = 0; i < requests; i++) {
+		v = memcached_fetch(&memc, NULL, NULL, &length, &flags, &ret);
 		if (ret != MEMCACHED_SUCCESS)
 			break;
 
-		printf("value len: %zd\n", value_length);
-		free(value);
-	} while (1);
+		if (value_length != length)
+			warnx("unexpected value length: %zd\n", length);
+
+		free(v);
+	}
+
+	if (i != requests)
+		warnx("%zd records lost", requests - i);
+
+	v = memcached_fetch(&memc, NULL, NULL, &length, &flags, &ret);
+	if (ret == MEMCACHED_SUCCESS)
+		warnx("unexpected record");
+	else if (ret != MEMCACHED_END)
+		warnx("unexpected error: %s", memcached_strerror(&memc, ret));
 
 	memcached_server_list_free(servers);
 	memcached_free(&memc);
 }
 
+static unsigned long value_length = 100UL;
+static unsigned long requests = 10000UL;
 static char *server = "127.0.0.1:21201";
 
 static void parse_options(int argc, char **argv)
 {
 	int c;
 
-	while ((c = getopt(argc, argv, "s:")) != -1) {
+	while ((c = getopt(argc, argv, "n:l:s:")) != -1) {
 		switch(c) {
+		case 'n':
+			requests = atol(optarg);
+			break;
+		case 'l':
+			value_length = atol(optarg);
+			break;
 		case 's':
 			server = optarg;
 			break;
@@ -92,7 +132,7 @@ static void parse_options(int argc, char **argv)
 int main(int argc, char **argv)
 {
 	parse_options(argc, argv);
-	run(server);
+	run(server, requests, value_length);
 
 	return 0;
 }
