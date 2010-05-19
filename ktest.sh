a13697698402e098e8ba1016092351b3cc45f509
Akinobu Mita <akinobu.mita@gmail.com>
2010-05-19 15:43:48 +0900

    update ktest.sh

--- ktest.sh
+++ ktest.sh
@@ -7,6 +7,8 @@ test_rodata
 testmmiotrace
 tcrypt
 kmemleak-test
+raid6test
+dmatest
 "
 
 for m in $MODULES; do
@@ -18,3 +20,17 @@ sleep 5
 modprobe -r rcutorture
 
 (cd ~/scm/linux-2.6/scripts/rt-tester; ./check-all.sh)
+
+cat << EOF > /dev/null
+test_suspend
+test_kprobes
+memtest
+pageattr-test
+kmemcheck/selftest
+atomic64_test
+trace_selftest
+drivers/net/sfc/selftest.c
+./drivers/md/raid6test/test.c
+Documentations
+EOF
+
